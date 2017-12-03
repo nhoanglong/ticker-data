580a778418075b71606fdfd895db266d4d6f82cd
nhoanglong <leonguyen0190@gmail.com>
2017-12-03 21:03:14 +0700

    Update unittest

--- src/test/java/sentifi/data/models/TWAPTest.java
+++ src/test/java/sentifi/data/models/TWAPTest.java
@@ -6,93 +6,90 @@ import junit.framework.TestSuite;
 
 public class TWAPTest extends TestCase {
 	private TWAP twap = new TWAP();
-	//4 days with open, close, high, low respectively
-	private static double[][] testData1 = { {1.0, 2.0, 3.0, 4.0}, 		   // day 1
-											{50.1, 20.2, 31.3, 23.7},	   // day 2
-											{25.1, 36.2, 24.3, 13.7},      // day 3
-											{150.1, 120.2, 131.3, 123.7}}; // day 4
-	//1 day
-	private static double[][] testData2 = {{50.1, 20.2, 31.3, 23.7}};
-	//3 days with open, close, high, low respectively
-	private static double[][] testData3 = { {100.1, 68.5, 98.0, 78.0}, // day 1
-											{50.1, 20.2, 31.3, 23.7},  // day 2
-											{25.1, 36.2, 24.3, 13.7}}; // day 3
+	// 4 days with open, close, high, low respectively
+	private static double[][] testData1 = { { 1.0, 2.0, 3.0, 4.0 }, // day 1
+			{ 50.1, 20.2, 31.3, 23.7 }, // day 2
+			{ 25.1, 36.2, 24.3, 13.7 }, // day 3
+			{ 150.1, 120.2, 131.3, 123.7 } }; // day 4
+	// 1 day
+	private static double[][] testData2 = { { 50.1, 20.2, 31.3, 23.7 } };
+	// 3 days with open, close, high, low respectively
+	private static double[][] testData3 = { { 100.1, 68.5, 98.0, 78.0 }, // day
+																			// 1
+			{ 50.1, 20.2, 31.3, 23.7 }, // day 2
+			{ 25.1, 36.2, 24.3, 13.7 } }; // day 3
 
 	/**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public TWAPTest( String testName )
-    {
-    	super( testName );
-    }
+	 * Create the test case
+	 *
+	 * @param testName
+	 *            name of the test case
+	 */
+	public TWAPTest(String testName) {
+		super(testName);
+	}
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( TWAPTest.class );
-    }
-    
-    private double[] prepareRecords(double[][] testData) {
-    	double[] expectedSum = {0.0, 0.0,0.0,0.0};
-    	
-    	for (int i = 0; i < testData.length; i++) {
-    		twap.addNewRecord(testData[i][0],testData[i][1],testData[i][2], testData[i][3]);
-    		for (int j = 0; j < 4; j++) {
-    			expectedSum[j] = expectedSum[j] + testData[i][j];
+	/**
+	 * @return the suite of tests being tested
+	 */
+	public static Test suite() {
+		return new TestSuite(TWAPTest.class);
+	}
+
+	private double[] prepareRecords(double[][] testData) {
+		double[] expectedSum = { 0.0, 0.0, 0.0, 0.0 };
+
+		for (int i = 0; i < testData.length; i++) {
+			twap.addNewRecord(testData[i][0], testData[i][1], testData[i][2], testData[i][3]);
+			for (int j = 0; j < 4; j++) {
+				expectedSum[j] = expectedSum[j] + testData[i][j];
 			}
 		}
-    	return expectedSum;
-    }
+		return expectedSum;
+	}
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testData1() 
-    {
-    	int numberOfRecords = testData1.length;
-    	double[] expectedSum = prepareRecords(testData1);
-    	assertEquals(twap.getOpenSum(), expectedSum[0]);
-    	assertEquals(twap.getCloseSum(), expectedSum[1]);
-    	assertEquals(twap.getHighSum(), expectedSum[2]);
-    	assertEquals(twap.getLowSum(), expectedSum[3]);
-    	
-    	assertEquals(twap.getTWAPOpen(), expectedSum[0]/numberOfRecords);
-    	assertEquals(twap.getTWAPClose(), expectedSum[1]/numberOfRecords);
-    	assertEquals(twap.getTWAPHigh(), expectedSum[2]/numberOfRecords);
-    	assertEquals(twap.getTWAPLow(), expectedSum[3]/numberOfRecords);
-    }
-    
-    public void testData2() 
-    {
-    	int numberOfRecords = testData2.length;
-    	double[] expectedSum = prepareRecords(testData2);
-    	assertEquals(twap.getOpenSum(), expectedSum[0]);
-    	assertEquals(twap.getCloseSum(), expectedSum[1]);
-    	assertEquals(twap.getHighSum(), expectedSum[2]);
-    	assertEquals(twap.getLowSum(), expectedSum[3]);
-    	
-    	assertEquals(twap.getTWAPOpen(), expectedSum[0]/numberOfRecords);
-    	assertEquals(twap.getTWAPClose(), expectedSum[1]/numberOfRecords);
-    	assertEquals(twap.getTWAPHigh(), expectedSum[2]/numberOfRecords);
-    	assertEquals(twap.getTWAPLow(), expectedSum[3]/numberOfRecords);
-    }
-    
-    public void testData3() 
-    {
-    	int numberOfRecords = testData3.length;
-    	double[] expectedSum = prepareRecords(testData3);
-    	assertEquals(twap.getOpenSum(), expectedSum[0]);
-    	assertEquals(twap.getCloseSum(), expectedSum[1]);
-    	assertEquals(twap.getHighSum(), expectedSum[2]);
-    	assertEquals(twap.getLowSum(), expectedSum[3]);
-    	
-    	assertEquals(twap.getTWAPOpen(), expectedSum[0]/numberOfRecords);
-    	assertEquals(twap.getTWAPClose(), expectedSum[1]/numberOfRecords);
-    	assertEquals(twap.getTWAPHigh(), expectedSum[2]/numberOfRecords);
-    	assertEquals(twap.getTWAPLow(), expectedSum[3]/numberOfRecords);
-    }
+	/**
+	 * Test with data.
+	 */
+	public void testData1() {
+		int numberOfRecords = testData1.length;
+		double[] expectedSum = prepareRecords(testData1);
+		assertEquals(twap.getOpenSum(), expectedSum[0]);
+		assertEquals(twap.getCloseSum(), expectedSum[1]);
+		assertEquals(twap.getHighSum(), expectedSum[2]);
+		assertEquals(twap.getLowSum(), expectedSum[3]);
+
+		assertEquals(twap.getTWAPOpen(), expectedSum[0] / numberOfRecords);
+		assertEquals(twap.getTWAPClose(), expectedSum[1] / numberOfRecords);
+		assertEquals(twap.getTWAPHigh(), expectedSum[2] / numberOfRecords);
+		assertEquals(twap.getTWAPLow(), expectedSum[3] / numberOfRecords);
+	}
+
+	public void testData2() {
+		int numberOfRecords = testData2.length;
+		double[] expectedSum = prepareRecords(testData2);
+		assertEquals(twap.getOpenSum(), expectedSum[0]);
+		assertEquals(twap.getCloseSum(), expectedSum[1]);
+		assertEquals(twap.getHighSum(), expectedSum[2]);
+		assertEquals(twap.getLowSum(), expectedSum[3]);
+
+		assertEquals(twap.getTWAPOpen(), expectedSum[0] / numberOfRecords);
+		assertEquals(twap.getTWAPClose(), expectedSum[1] / numberOfRecords);
+		assertEquals(twap.getTWAPHigh(), expectedSum[2] / numberOfRecords);
+		assertEquals(twap.getTWAPLow(), expectedSum[3] / numberOfRecords);
+	}
+
+	public void testData3() {
+		int numberOfRecords = testData3.length;
+		double[] expectedSum = prepareRecords(testData3);
+		assertEquals(twap.getOpenSum(), expectedSum[0]);
+		assertEquals(twap.getCloseSum(), expectedSum[1]);
+		assertEquals(twap.getHighSum(), expectedSum[2]);
+		assertEquals(twap.getLowSum(), expectedSum[3]);
+
+		assertEquals(twap.getTWAPOpen(), expectedSum[0] / numberOfRecords);
+		assertEquals(twap.getTWAPClose(), expectedSum[1] / numberOfRecords);
+		assertEquals(twap.getTWAPHigh(), expectedSum[2] / numberOfRecords);
+		assertEquals(twap.getTWAPLow(), expectedSum[3] / numberOfRecords);
+	}
 }
