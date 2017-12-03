1890a049b2fa013de2d08c59457398d5ab1d3d7b
nhoanglong <leonguyen0190@gmail.com>
2017-12-03 09:58:28 +0700

    Add cmt

--- src/main/java/sentifi/data/models/VolumeAverage.java
+++ src/main/java/sentifi/data/models/VolumeAverage.java
@@ -1,5 +1,13 @@
 package sentifi.data.models;
 
+/**
+ * Volume Average model.
+ * Extends and re-uses from Simple Moving Average model due to many common attributes.
+ * 
+ * @author nhoanglong
+ * @since Dec 03 2017
+ * @version 1.0
+ */
 public class VolumeAverage extends SimpleMovingAverage {
 
 	public VolumeAverage(int length, String name) {
